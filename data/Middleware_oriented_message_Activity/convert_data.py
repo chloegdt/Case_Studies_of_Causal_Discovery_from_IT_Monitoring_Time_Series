998841d3cef3c1380e3a7886f6f9b59de9c3904e
Alessandro Leite <alessandro.leite@lisn.fr>
2024-06-23 16:31:34 +0200

    Fix the code to include the path of the script

--- data/Middleware_oriented_message_Activity/convert_data.py
+++ data/Middleware_oriented_message_Activity/convert_data.py
@@ -1,14 +1,17 @@
+import os
 import pandas as pd
 from datetime import datetime
 
-df = pd.read_csv('monitoring_metrics_1.csv')
+path_file = os.path.dirname(os.path.realpath(__file__))
+
+df = pd.read_csv(f'{path_file}/monitoring_metrics_1.csv')
 
 # Conversion timestamp en format datetime
 df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
 
-#  YY-MM-DD HH:MM:SS
-df['timestamp'] = df['timestamp'].dt.strftime('%y-%m-%d %H:%M:%S')
+#  YYYY-MM-DD HH:MM:SS
+df['timestamp'] = df['timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
 
-# enrigstrement d'un nouveau fichier CSV
-df.to_csv('monitoring_metrics_1bis.csv', index=False)
+# enregistrement d'un nouveau fichier CSV
+df.to_csv(f'{path_file}/monitoring_metrics_1bis.csv', index=False)
 
